071cf24a1db4a4590ef23734f4fb1d5ebd4ea82e
Chris Esplin <chris@quiver.is>
2014-12-04 10:53:19 -0700

    Committing mock env.js

---
+++ test/mock/env/env.js
@@ -0,0 +1,29 @@
+window.envVars = {
+    "environment":"development",
+    "html5Mode":false,
+    "firebase":{"endpoint":"https://dev-quiver.firebaseio.com/quiver-cms-test"},
+    "api":"https://dev-quiver-cms.quiver.is/api",
+    "root":"https://dev-quiver-cms.quiver.is",
+    "email":{"from":"chris@quiver.is","name":"Chris Esplin"},
+    "imageSizes":{"small":640,"medium":1024,"large":1440,"xlarge":1920,"postSize":"medium"},
+    "supportedImageTypes":["jpg","jpeg","png","gif","tiff"],
+    "supportedVideoTypes":["mp4","webm","mpeg","ogg"],
+    "maps":{"apiKey":"GREATDAYALL"},
+    "disqus":{"shortname":"test"},
+    "amazon":{"publicBucket":"assets.test.com"},
+    "content":{"redisEnabled":false},
+    "rss":{
+      "title":"Quiver CMS",
+      "description":"A Firebase/Express/Angular/Node CMS",
+      "link":"https://quiver.is/",
+      "image":"https://quiver.is/small.jpg",
+      "docs":"https://quiver.is/my-rss-documentation",
+      "managingEditor":"Chris Esplin: chris@quiver.is",
+      "webMaster":"Chris Esplin: chris@quiver.is",
+      "copyright":"MIT",
+      "language":"english",
+      "categories":["cats","dogs"],
+      "ttl":3600
+    },
+    "google":{"analyticsId":"UA-6859198-XX"}
+  };
