6b1bb3b684b5a5a8fc93f9c3e0106b09121a6e60
Chris Esplin <chris@quiver.is>
2015-04-29 08:32:04 -0600

    Materialize settings.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,61 +1,33 @@
 'use strict';
 
 angular.module('quiverCmsApp')
-  .controller('AdminCtrl', function ($scope, theme, settings, adminSettings, AdminService, ObjectService, NotificationService) {
+    .controller('AdminCtrl', function($scope, theme, settings, adminSettings, AdminService, ObjectService, NotificationService) {
 
-    /*
-     * Theme
-    */
-    theme.$bindTo($scope, 'theme');
-    ObjectService.toDestroy(theme);
+        /*
+         * Theme
+         */
+        theme.$bindTo($scope, 'theme');
+        ObjectService.toDestroy(theme);
 
-    theme.$loaded().then(function (theme) {
-      if (theme.options) {
-        var keys = Object.keys($scope.theme.options);
+        theme.$loaded().then(function(theme) {
+            if (theme.options) {
+                var keys = Object.keys($scope.theme.options);
 
-        if (!$scope.theme.active && keys.length) {
-          $scope.theme.active = keys[0];
-        }
+                if (!$scope.theme.active && keys.length) {
+                    $scope.theme.active = keys[0];
+                }
 
-      }
-      
+            }
+        });
+
+        /*
+         * Settings
+         */
+        settings.$bindTo($scope, 'settings');
+
+        /*
+         * Admin Settings
+         */
+        adminSettings.$bindTo($scope, 'adminSettings');
 
     });
-
-    /*
-     * Settings
-    */
-    settings.$bindTo($scope, 'settings');
-
-    $scope.setNavTitle = function (name, value) {
-      $scope.settings[name + 'Title'] = value && value.length ? value : null;
-    };
-
-    $scope.addNavLink = function (type, link) {
-      var list = $scope.settings[type];
-      if (!list || !list.length || typeof list !== 'object') {
-        list = [];
-      }
-
-
-
-      list.push(link);
-
-      $scope.settings[type] = list;
-    };
-
-    $scope.removeNavLink = function (type, i) {
-      var list = $scope.settings[type];
-      list.splice(i, 1);
-
-      settings[type] = list;
-      settings.$save();
-
-    };
-
-    /*
-     * Admin Settings
-     */
-    adminSettings.$bindTo($scope, 'adminSettings');
-
-  });
--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -8,45 +8,71 @@
  * Controller of the quiverCmsApp
  */
 angular.module('quiverCmsApp')
-  .controller('SettingsCtrl', function ($scope) {
-    $scope.navList = [
-      {
-        name: "Nav #1",
-        slug: "nav1"
-      },
-      {
-        name: "Nav #2",
-        slug: "nav2"
-      },
-      {
-        name: "Nav #3",
-        slug: "nav3"
-      },
-      {
-        name: "Footer #1",
-        slug: "footer1"
-      },
-      {
-        name: "Footer #2",
-        slug: "footer2"
-      },
-      {
-        name: "Footer #3",
-        slug: "footer3"
-      },
-      {
-        name: "App #1",
-        slug: "app1"
-      },
-      {
-        name: "App #2",
-        slug: "app2"
-      },
-      {
-        name: "App #3",
-        slug: "app3"
-      },
+    .controller('SettingsCtrl', function($scope, $timeout) {
+        $scope.navList = [{
+                name: "Nav #1",
+                slug: "nav1"
+            }, {
+                name: "Nav #2",
+                slug: "nav2"
+            }, {
+                name: "Nav #3",
+                slug: "nav3"
+            }, {
+                name: "Footer #1",
+                slug: "footer1"
+            }, {
+                name: "Footer #2",
+                slug: "footer2"
+            }, {
+                name: "Footer #3",
+                slug: "footer3"
+            }, {
+                name: "App #1",
+                slug: "app1"
+            }, {
+                name: "App #2",
+                slug: "app2"
+            }, {
+                name: "App #3",
+                slug: "app3"
+            },
 
-    ];
+        ];
 
-  });
+        $scope.cleanNavArray = function(nav) {
+            $timeout(function() {
+                var i = nav.length;
+
+                while (i--) {
+                    if (!nav[i] || !nav[i].href || !nav[i].text) {
+                        nav.splice(i, 1);
+                    }
+                }
+            });
+
+        };
+
+        $scope.addNavLink = function(type, link) {
+            var list = $scope.settings[type];
+            if (!list || !list.length || typeof list !== 'object') {
+                list = [];
+            }
+
+
+
+            list.push(link);
+
+            $scope.settings[type] = list;
+        };
+
+        $scope.removeNavLink = function(type, i) {
+            var list = $scope.settings[type];
+            list.splice(i, 1);
+
+            settings[type] = list;
+            settings.$save();
+
+        };
+
+    });
