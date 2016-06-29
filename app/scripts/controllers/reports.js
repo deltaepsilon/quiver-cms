434b18848230cd6315c3d44f476c8d3c26650d3b
Chris Esplin <chris@quiver.is>
2016-06-28 19:10:20 -0600

    Sale By Day and Product report

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -8,24 +8,24 @@
  * Controller of the quiverCmsApp
  */
 angular.module('quiverCmsApp')
-    .controller('ReportsCtrl', function($scope, reports, AdminService, NotificationService, _) {
+    .controller('ReportsCtrl', function ($scope, reports, AdminService, NotificationService, _) {
         /*
          * Reports
          */
-        reports.$loaded().then(function(reports) {
+        reports.$loaded().then(function (reports) {
             $scope.reports = {
                 discounts: reports.discounts,
                 surveys: reports.surveys,
                 sales: {
                     created: reports.sales ? reports.sales.created : false
-                }
-            }
-            _.each(reports.sales, function(value, key) {
+                },
+                byDayByProduct: reports.byDayByProduct
+            };
+            _.each(reports.sales, function (value, key) {
                 if (typeof value === 'object') {
                     $scope.reports.sales[key] = _.toArray(value);
                 }
             });
-
         });
 
         $scope.salesReportTypes = {
@@ -35,16 +35,47 @@ angular.module('quiverCmsApp')
             byDay: 'Day'
         };
 
-        $scope.runReports = function() {
+        $scope.setProductSelected = function (slug) {
+            if (!slug) {
+                return [];
+            } else {
+                var unsorted = _.map($scope.reports.byDayByProduct.byDay, function (report, date) {
+                    if (!report.day[slug]) {
+                        return undefined;
+                    } else {
+                        return {
+                            date: date,
+                            day: report.day[slug],
+                            month: report.month[slug],
+                            year: report.year[slug],
+                            total: report.total[slug]
+                        };
+                    }
+                });
+                var i = unsorted.length;
+                
+
+                while (i--) {
+                    if (!unsorted[i]) {
+                        unsorted.splice(i, 1);
+                    }
+                }
+
+                $scope.byDay = unsorted.sort(function (a, b) {
+                    return a.date < b.date;
+                });
+            }
+        };
+
+        $scope.runReports = function () {
             $scope.runningReports = true;
-            AdminService.runReports().then(function() {
+            AdminService.runReports().then(function () {
                 NotificationService.success('Reports Run!');
                 delete $scope.runningReports;
                 location.reload();
-            }, function(error) {
+            }, function (error) {
                 NotificationService.error('Reports Error', error);
                 delete $scope.runningReports;
             });
-
         };
     });
