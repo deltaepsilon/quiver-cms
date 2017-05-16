8009de15ca5b005016aeaeeea05541eb051daf9d
Chris Esplin <chris@quiver.is>
2017-05-16 05:54:06 -0600

    Adding optimize snippet.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,6 +26,9 @@ angular.module('quiverCmsApp', [
                 }
 
                 Analytics.createAnalyticsScriptTag(config);
+                if (env.google.optimizeId && typeof ga == 'function') {
+                    ga('require', env.google.optimizeId);
+                }
                 stateChangeSuccessOff();
             });
 
